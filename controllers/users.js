a813cf62e05be487be12a6d2428fd054dc62a565
Vadim-Vatrya <VatryaVV@gmail.com>
2021-05-23 00:46:50 +0300

    step 3 and 4

--- controllers/users.js
+++ controllers/users.js
@@ -36,15 +36,41 @@ const login = async (req, res, next) => {
     const {email, password} = req.body
     const user = await Users.findByEmail(email)
     const isValidPassword = await user?.ValidPassword(password)
+
     if (!user || !isValidPassword) {
-      
-    }
-  } catch(e) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        message: 'Email or password is wrong',
+    })
+  } 
+
+  const id = user.id
+  const payload = { id }
+  const token = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '2h' })
+  await Users.updateToken(id, token)
+  return res.status(HttpCode.OK).json({
+    status: "success",
+    code: HttpCode.OK,
+    data: {
+      token,
+    },
+  })
+}
+  catch(e) {
     next(e)
   }
 };
 
-const logout = async (req, res, next) => {};
+const logout = async (req, res, next) => {
+  const id = req.user?.id
+  const user = await Users.getUserById(id)
+  if (!user) {
+    return res.status(HttpCode.UNAUTHORIZED).json({
+      message: 'Not authorized',
+    })
+  }
+  await Users.updateToken(id, null)
+  return res.status(HttpCode.NO_CONTENT).json({})
+};
 
 module.exports = {
   register,
