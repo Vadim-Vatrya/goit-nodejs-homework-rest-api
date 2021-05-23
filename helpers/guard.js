2981ce75c735479c5675b885f809797d208ed946
Vadim-Vatrya <VatryaVV@gmail.com>
2021-05-23 13:03:04 +0300

    connect passport in guard

--- helpers/guard.js
+++ helpers/guard.js
@@ -1,8 +1,24 @@
 const passport = require('passport')
-
+const { HttpCode } = require('./constans')
+require('../config/passport')
 
 const guard = (req, res, next) => {
-  console.log('auth');
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+
+    const [, token] = req.get('Authorization').split(' ')
+
+    if (!user || err || token !== user.token) {
+        return res.status(HttpCode.UNAUTHORIZED).json({
+            status: 'error',
+            code: HttpCode.UNAUTHORIZED,
+            data: 'Unauthorized',
+            message: 'Not authorized',
+        })
+    }
+
+    req.user = user
+    return next()
+  })(req, res, next)
   next()
 }
 
