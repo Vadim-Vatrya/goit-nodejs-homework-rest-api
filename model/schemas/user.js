391169a57d7a84f3e3add2d5322c4a3afcb2b9d5
Vadim-Vatrya <VatryaVV@gmail.com>
2021-06-09 05:46:05 +0300

    step 1

--- model/schemas/user.js
+++ model/schemas/user.js
@@ -1,67 +1,80 @@
-const mongoose = require('mongoose')
-const{ Schema } = mongoose
-const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs')
+const { Schema, model } = require('mongoose')
+const gravatar = require("gravatar")
+const bcrypt = require("bcryptjs")
+const { nanoid } = require('nanoid')
 const SALE_FACTOR = 6
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    validate(value) {
-      const re = /\S+@\S+\.\S+/
-      return re.test(String(value).toLowerCase())
-    }
-  },
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate(value) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(String(value).toLowerCase());
+      },
+    },
 
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: 6,
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+
+    userIdImg: {
+      type: String,
+      default: null,
+    },
+
+    avatar: {
+      type: String,
+      default: function () {
+        return gravatar.url(this.email, { s: 250 }, true);
+      },
+    },
+
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+
+    verifyToken: {
+      type: String,
+      required: true,
+      default: nanoid(),
+    },
+
+    token: {
+      type: String,
+      default: null,
+    },
 
   },
-
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter"
-  },
-
-  userIdImg: {
-    type: String,
-    default: null,
-  },
-
-  avatar: {
-    type: String,
-    default: function () {
-      return gravatar.url(this.email, { s: 250 }, true)
-    }
-  },
-  
-  token: {
-    type: String,
-    default: null,
-  },
-},
-{
-  versionKey: false,
-  timestamps: true,
-})
-
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt(SALE_FACTOR)
-  this.password = await bcrypt.hash(this.password, salt)
+  {
+    versionKey: false,
+    timestamps: true,
   }
-  next()
-})
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(SALE_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
 
 userSchema.methods.validPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model('user', userSchema)
+const User = model("user", userSchema);
 
-module.exports = User
+module.exports = User;
